181e4085f5399dbf552d8a9d4919368a28c63476
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2021-06-30 21:19:22 -0300

    commit

--- lib/blocs/user_bloc.dart
+++ lib/blocs/user_bloc.dart
@@ -25,17 +25,52 @@ class UserBloc extends BlocBase {
         switch(change.type){
           case DocumentChangeType.modified:
             _users[uid] =  change.doc.data as Map<String, dynamic>;
+            _subscribeToOrders(uid);
             //_users[uid] = change.doc.data();
             break;
           case DocumentChangeType.modified:
             _users[uid]!.addAll(change.doc.data as Map<String, dynamic>);
+            _usersController.add(_users.values.toList());
             break;
           case DocumentChangeType.removed:
             _users.remove(uid);
+            _unsubscribeToOrders(uid);
+            _usersController.add(_users.values.toList());
             break;
         }
       });
     });
+  }
+
+  void _subscribeToOrders(String uid) {
+    _users[uid]["subscription"] = _firestore.collection("users").doc(uid).collection("orders")
+        .snapshots().listen((orders) async {
+
+          int numOrders = orders.docs.length;
+
+          double money = 0.0;
+
+          for(DocumentSnapshot d in orders.docs){
+            DocumentSnapshot order = await _firestore.collection("orders")
+                .doc(d.id).get();
+
+            if(order.data() == null) continue;
+
+            // precisa de dados primeiro
+            //money += order.data["totalPrice"];
+          }
+
+          _users[uid]!.addAll(
+            {"money": money, "orders": numOrders}
+          );
+
+          _users[uid]!.addAll(_users.values.toList() as Map<String, dynamic>);
+
+    });
+  }
+
+  void _unsubscribeToOrders(String uid){
+    _users[uid]["subscription"].cancel();
   }
 
   @override
